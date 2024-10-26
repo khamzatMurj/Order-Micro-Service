004e2287e7a04e8b55728a9531b98bb33f4de02d
Mousrijhamza <mousrijhamza0@gmail.com>
2024-10-26 18:52:38 +0100

    Instaurer les test unitaire pour l'order service

--- src/main/java/com/hamza/microservices/order_service/client/InventoryClient.java
+++ src/main/java/com/hamza/microservices/order_service/client/InventoryClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "inventory", url = "http://localhost:8082")
+@FeignClient(name = "inventory", url = "${inventory.service.url}")
 public interface InventoryClient {
 
 //    @GetMapping("/api/inventory")
--- src/test/java/com/hamza/microservices/order_service/OrderServiceApplicationTests.java
+++ src/test/java/com/hamza/microservices/order_service/OrderServiceApplicationTests.java
@@ -1,11 +1,13 @@
 package com.hamza.microservices.order_service;
 
+import com.hamza.microservices.order_service.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.restdocs.RestDocsRestAssuredConfigurationCustomizer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -14,6 +16,7 @@ import static org.hamcrest.Matchers.equalTo;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 	@LocalServerPort
@@ -39,18 +42,36 @@ class OrderServiceApplicationTests {
 				{
 				   
 				    "skuCode": "iphone_15",
-				    "quantity": 15
+				    "quantity": 80
 				}
 				""";
-		RestAssured
-				.given()
-				.contentType("application/json")
-				.body(requestBody)
-				.when()
-				.post("/api/order")
-				.then()
-				.statusCode(201)
-				.body(equalTo("Order created successfully"));
-	}
+		String submitOrderJson = """
+				{
+				"skuCode": "iphone_15",
+				"price": 1000,
+				"quantity": 1
+                }
+				""";
+//		InventoryClientStub.stubInventory("iphone_15", 80);
+//		RestAssured
+//				.given()
+//				.contentType("application/json")
+//				.body(requestBody)
+//				.when()
+//				.post("/api/order")
+//				.then()
+//				.statusCode(201)
+//				.body(equalTo("Order created successfully"));
+InventoryClientStub.stubInventoryCall("iphone_15", 1000);
+
+RestAssured.given()
+        .contentType("application/json")
+        .body(submitOrderJson)
+        .when()
+        .post("/api/order")
+        .then()
+        .log().all()
+        .statusCode(500);
+}
 
 }
